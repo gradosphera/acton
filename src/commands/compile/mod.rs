b2eed0e3c4e304e88f39a1dadab726eb99200260
i582 <51853996+i582@users.noreply.github.com>
2025-10-30 04:55:51 +0400

    output code in hex in compile command

--- src/commands/compile/mod.rs
+++ src/commands/compile/mod.rs
@@ -3,6 +3,7 @@ use owo_colors::OwoColorize;
 use serde_json;
 use std::fs;
 use std::path::Path;
+use tycho_types::boc::Boc;
 
 pub fn compile_cmd(path: &String, json: bool, base64_only: bool) -> Result<(), anyhow::Error> {
     let metadata = fs::metadata(path)?;
@@ -22,18 +23,23 @@ pub fn compile_cmd(path: &String, json: bool, base64_only: bool) -> Result<(), a
 
     match compilation_result {
         tolkc::CompilerResult::Success(result) => {
+            let code = Boc::decode_base64(result.code_boc64.clone())?;
+            let code_hex = Boc::encode_hex(code);
+
             if base64_only {
                 println!("{}", result.code_boc64);
             } else if json {
                 let json_output = serde_json::json!({
                     "success": true,
                     "code_boc64": result.code_boc64,
+                    "code_hex": code_hex,
                     "code_hash_hex": result.code_hash_hex
                 });
                 println!("{}", serde_json::to_string_pretty(&json_output).unwrap());
             } else {
                 println!("{}", "✓ Compilation successful".green().bold());
                 println!("Code in base64: {}", result.code_boc64.dimmed());
+                println!("Code in hex: {}", code_hex.dimmed());
                 println!("Code hash hex: {}", result.code_hash_hex.dimmed());
             }
             Ok(())
