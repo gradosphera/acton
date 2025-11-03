77c288af81e2749d1757dfe1887ebf8d66629043
i582 <51853996+i582@users.noreply.github.com>
2025-11-03 12:29:57 +0400

    fix coverage collection

--- src/commands/test/coverage.rs
+++ src/commands/test/coverage.rs
@@ -6,7 +6,7 @@ use owo_colors::OwoColorize;
 use std::collections::{HashMap, HashSet};
 use std::fs;
 use std::path::Path;
-use tolkc::source_map::DebugLocation;
+use tolkc::source_map::EntryContextDescription;
 
 #[derive(Debug, Clone)]
 pub struct Coverage {
@@ -19,7 +19,7 @@ pub struct FileCoverage {
     pub executable_lines: i64,
     pub covered_lines: i64,
     pub line_hits: HashMap<i64, u64>, // line number -> hit count
-    pub executable_line_numbers: Vec<i64>, // all executable line numbers
+    pub executable_line_numbers: HashSet<i64>, // all executable line numbers
 }
 
 pub fn collect_coverage(emulations: &Emulations, build_cache: &BuildCache) -> Coverage {
@@ -30,8 +30,7 @@ pub fn collect_coverage(emulations: &Emulations, build_cache: &BuildCache) -> Co
     });
 
     let mut seen_source_maps = HashSet::new();
-    let mut whole_executable_locations_per_file: HashMap<String, Vec<DebugLocation>> =
-        HashMap::new();
+    let mut whole_executable_lines_per_file: HashMap<String, HashSet<i64>> = HashMap::new();
 
     let mut whole_trace = vec![];
 
@@ -51,7 +50,7 @@ pub fn collect_coverage(emulations: &Emulations, build_cache: &BuildCache) -> Co
             continue;
         }
 
-        let mut executable_locations_per_file: HashMap<String, Vec<DebugLocation>> = HashMap::new();
+        let mut executable_lines_per_file: HashMap<String, HashSet<i64>> = HashMap::new();
         let source_maps_locations = source_map.high_level.locations;
         let executable_locations = source_maps_locations;
         for loc in executable_locations {
@@ -65,23 +64,30 @@ pub fn collect_coverage(emulations: &Emulations, build_cache: &BuildCache) -> Co
             }
 
             let file = loc.loc.file.clone();
-            if whole_executable_locations_per_file.contains_key(&file) {
+            if whole_executable_lines_per_file.contains_key(&file) {
                 // we already have executable lines for this file
                 continue;
             }
 
-            let entry = executable_locations_per_file
+            if let EntryContextDescription::Basic { ast_kind } = loc.context.description
+                && ast_kind == "ast_block_statement"
+            {
+                // skip block statements
+                continue;
+            }
+
+            let entry = executable_lines_per_file
                 .entry(file)
-                .or_insert_with(Vec::new);
-            entry.push(loc);
+                .or_insert_with(HashSet::new);
+            entry.insert(loc.loc.line);
         }
 
-        for (path, locs) in &executable_locations_per_file {
-            if whole_executable_locations_per_file.contains_key(path) {
+        for (path, locs) in &executable_lines_per_file {
+            if whole_executable_lines_per_file.contains_key(path) {
                 // we already have executable lines for this file
                 continue;
             }
-            whole_executable_locations_per_file.insert(path.clone(), locs.clone());
+            whole_executable_lines_per_file.insert(path.clone(), locs.clone());
         }
     }
 
@@ -100,16 +106,6 @@ pub fn collect_coverage(emulations: &Emulations, build_cache: &BuildCache) -> Co
     let mut coverages: Vec<FileCoverage> = vec![];
 
     let mut line_hits_per_file: HashMap<String, HashMap<i64, u64>> = HashMap::new();
-    let mut executable_lines_per_file: HashMap<String, Vec<i64>> = HashMap::new();
-
-    for (file, locations) in &whole_executable_locations_per_file {
-        let mut unique_lines = HashSet::new();
-        for loc in locations {
-            unique_lines.insert(loc.loc.line);
-        }
-        let executable_line_numbers: Vec<i64> = unique_lines.into_iter().collect();
-        executable_lines_per_file.insert(file.clone(), executable_line_numbers);
-    }
 
     for loc in &whole_trace {
         let file = &loc.loc.file;
@@ -125,29 +121,25 @@ pub fn collect_coverage(emulations: &Emulations, build_cache: &BuildCache) -> Co
         .map(|loc| loc.loc.line)
         .collect::<HashSet<_>>();
 
-    for (file, locations) in &whole_executable_locations_per_file {
-        let executable_lines = locations.len() as i64;
+    for (file, lines) in whole_executable_lines_per_file {
+        let executable_lines = lines.len() as i64;
         let mut covered_lines = 0;
 
-        for file_loc in locations {
-            let covered = executed_lines.contains(&file_loc.loc.line);
+        for line in &lines {
+            let covered = executed_lines.contains(&line);
             if covered {
                 covered_lines += 1
             }
         }
 
-        let line_hits = line_hits_per_file.get(file).cloned().unwrap_or_default();
-        let executable_line_numbers = executable_lines_per_file
-            .get(file)
-            .cloned()
-            .unwrap_or_default();
+        let line_hits = line_hits_per_file.get(&file).cloned().unwrap_or_default();
 
         coverages.push(FileCoverage {
             file: file.clone(),
             executable_lines,
             covered_lines,
             line_hits,
-            executable_line_numbers,
+            executable_line_numbers: lines,
         })
     }
 
