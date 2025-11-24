05e82ccd9ddedd6ad118d3b868e37491c61d7c7d
i582 <51853996+i582@users.noreply.github.com>
2025-11-24 12:34:11 +0400

    add debugget tests for match over lazy message

--- tests/debugging/debug_test.rs
+++ tests/debugging/debug_test.rs
@@ -220,6 +220,159 @@ fun main() {
 }
 
 #[test]
+fn test_match_over_lazy_message_first_matching() -> anyhow::Result<()> {
+    let code = r#"
+struct (0x00000001) First {
+    id: int32
+}
+struct (0x00000002) Second {
+    data: bool
+}
+
+type Msg = First | Second
+
+fun main() {
+    val msg = lazy Msg.fromCell(First { id: 10 }.toCell());
+    match (msg) {
+        First => {
+            return 10
+        }
+        Second => {
+            return 20
+        }
+        else => {
+            throw 0xFFFF
+        }
+    }
+
+    return 0;
+}
+"#;
+
+    let session = DebugBuilder::new("debug-callback").code(code).build();
+
+    let mut client = session.start();
+
+    let result = client.execute(|executor| {
+        executor.step_over()?;
+        executor.step_over()?;
+        Ok(())
+    })?;
+
+    let debug_output = DebugTestOutput::new(result);
+    debug_output.assert_trace_snapshot_matches(
+        "debugging/snapshots/match/over_lazy_message_first_matching.trace.txt",
+    );
+
+    Ok(())
+}
+
+#[test]
+fn test_match_over_lazy_message_second_matching() -> anyhow::Result<()> {
+    let code = r#"
+struct (0x00000001) First {
+    id: int32
+}
+struct (0x00000002) Second {
+    data: bool
+}
+
+type Msg = First | Second
+
+fun main() {
+    val msg = lazy Msg.fromCell(Second { data: true }.toCell());
+    match (msg) {
+        First => {
+            return 10
+        }
+        Second => {
+            return 20
+        }
+        else => {
+            throw 0xFFFF
+        }
+    }
+
+    return 0;
+}
+"#;
+
+    let session = DebugBuilder::new("debug-callback").code(code).build();
+
+    let mut client = session.start();
+
+    let result = client.execute(|executor| {
+        executor.step_over()?;
+        executor.step_over()?;
+        executor.step_over()?;
+        executor.step_over()?;
+        Ok(())
+    })?;
+
+    // TODO
+    let debug_output = DebugTestOutput::new(result);
+    debug_output.assert_trace_snapshot_matches(
+        "debugging/snapshots/match/over_lazy_message_second_matching.trace.txt",
+    );
+
+    Ok(())
+}
+
+#[test]
+fn test_match_over_lazy_message_else_matching() -> anyhow::Result<()> {
+    let code = r#"
+struct (0x00000001) First {
+    id: int32
+}
+struct (0x00000002) Second {
+    data: bool
+}
+struct (0x00000003) Third
+
+type Msg = First | Second
+
+fun main() {
+    val msg = lazy Msg.fromCell(Third {}.toCell());
+    match (msg) {
+        First => {
+            return 10
+        }
+        Second => {
+            return 20
+        }
+        else => {
+            throw 0xFFFF
+        }
+    }
+
+    return 0;
+}
+"#;
+
+    let session = DebugBuilder::new("debug-callback").code(code).build();
+
+    let mut client = session.start();
+
+    let result = client.execute(|executor| {
+        executor.step_over()?;
+        executor.step_over()?;
+        executor.step_over()?;
+        executor.step_over()?;
+        executor.step_over()?;
+        executor.step_over()?;
+        Ok(())
+    })?;
+
+    // TODO
+    let debug_output = DebugTestOutput::new(result);
+    debug_output.assert_trace_snapshot_matches(
+        "debugging/snapshots/match/over_lazy_message_else_matching.trace.txt",
+    );
+
+    Ok(())
+}
+
+#[test]
 fn test_if_over_numbers_with_first_matching() -> anyhow::Result<()> {
     let code = r#"
 global foo: int;
