7c0c9d880b4e9e40070d99caaef36802db00c332
Petr Makhnev <51853996+i582@users.noreply.github.com>
2026-04-19 19:37:47 +0400

    feat(docs): better styles for flags and arguments in command reference

--- docs/mdx-components.tsx
+++ docs/mdx-components.tsx
@@ -2,6 +2,7 @@ import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 import {
   CommandOption,
+  CommandOptionMeta,
   CommandOptions,
   CommandOptionTitle,
 } from '@/components/CommandOptions';
@@ -10,6 +11,7 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     CommandOption,
+    CommandOptionMeta,
     CommandOptions,
     CommandOptionTitle,
     ...components,
